277be5b61f68969157ae9e2be4fa377fca8842c3
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-05-03 23:00:46 +0100

    stupid everything

--- appveyor.sh
+++ appveyor.sh
@@ -1,2 +1,4 @@
 #https://github.com/NixOS/nix/issues/2292#issuecomment-443933924
-sudo mkdir /etc/nix; echo 'use-sqlite-wal = false' | sudo tee -a /etc/nix/nix.conf && sh <(curl https://nixos.org/nix/install)
+sudo mkdir /etc/nix;
+echo 'use-sqlite-wal = false' | sudo tee -a /etc/nix/nix.conf
+echo 'sandbox = false' | sudo tee -a /etc/nix/nix.conf
