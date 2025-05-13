32fb112e4b6dbc15690ce27f6f6e93349534e041
maralorn <mail@maralorn.de>
2025-05-13 18:18:06 +0200

    Deactivate libiconv override
    Causes infinite recursion
    
    According to @alexfmpe unnecessary since: https://github.com/NixOS/nixpkgs/pull/355543

--- default.nix
+++ default.nix
@@ -170,10 +170,6 @@ let iosSupport = system == "x86_64-darwin";
             '';
           });
 
-          libiconv = super.libiconv.overrideAttrs (old: lib.optionalAttrs (self.stdenv.hostPlatform.useAndroidPrebuilt or false) {
-            configureFlags = [ "--disable-shared" "--enable-static" ];
-          });
-
           libffi = if (self.stdenv.hostPlatform.useAndroidPrebuilt or false) then super.libffi_3_3 else super.libffi;
         })
       ] ++ nixpkgsOverlays;
